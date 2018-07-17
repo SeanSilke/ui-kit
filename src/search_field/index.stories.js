742bfc5145fab89596a8a2bf81235460b1da21bb
Sergey Orlov <OrlovMail@gmail.com>
2018-07-17 21:38:41 +0300

    EN-333 Added search_field component

---
+++ src/search_field/index.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { SearchField } from './index';
+import { MUThemeDecorator } from '../../../.storybook/mu-theme-decorator';
+
+const vraperProps = {
+  style: {
+    width: '50%'
+  }
+};
+
+storiesOf('SearchField', module)
+  .addDecorator(MUThemeDecorator)
+  .addDecorator(story => <div {...vraperProps}>{story()} </div>)
+  .add('Uncontrolled Component', () => (
+    <SearchField floatingLabelText="Найти профиль" name="search" onChange={action('onChange')} />
+  ))
+  .add('Uncontrolled Component with initial value', () => (
+    <SearchField
+      floatingLabelText="Найти профиль"
+      defaultValue="defaultValue"
+      name="search"
+      onChange={action('onChange')}
+    />
+  ))
+  .add('Controlled component with initial value', () => (
+    <SearchField floatingLabelText="Найти профиль" name="search" value={'some value'} onChange={action('onChange')} />
+  ));
