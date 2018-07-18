617b20e56f4b3f917f5ecbec713a8a9e05cea31b
Sergey Orlov <OrlovMail@gmail.com>
2018-07-18 15:35:17 +0300

    EN-331 add select_multiple

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default as Fancy } from './fancy';
 export { HighlightText } from './highlight-text';
 export { SearchField } from './search_field';
+export { ListItemLink } from './list-item-link';
+export { SelectMultiple } from './select_multiple';
