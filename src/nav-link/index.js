ea51f99ce835329f7bd251bf496d9f131208d476
Sergey Orlov <orlovMail@gmail.com>
2018-08-02 18:14:54 +0300

    EN-331 removed all navigation component, This is escape from react-router dependency'

--- src/nav-link/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { NavLink as _NavLink } from 'react-router-dom';
-import { isLinkActive } from './helpers';
-
-export const NavLink = ({ to, ...props }) => <_NavLink to={to} isActive={isLinkActive(to)} {...props} />;
--- src/nav-menu/proptypes.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const itemShape = PropTypes.shape({
-  text: PropTypes.string.isRequired,
-  to: PropTypes.string.isRequired
-});
