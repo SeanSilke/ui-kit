ee699639739a96db8104f853c9954febb2cce3d4
Sergey Orlov <OrlovMail@gmail.com>
2018-08-31 13:30:20 +0300

    EN-334 Added addon jest to sotybook

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # modelViewer-UI
+
+## Generating the test results
+
+You need to make sure the generated test-restuls file exists before you start storybook.
+During development you will likely start jest in watch-mode
+and so the json file will be re-generated every time code or tests change.
+
+```sh
+npm run test:generate-output -- --watch
+```
---
+++ src/jest-test-example/index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import results from '../../jest-test-results.json';
+import { storiesOf } from '@storybook/react';
+import { withTests } from '@storybook/addon-jest';
+
+// NOTE: We use the old version of storybook the documentation is here
+//https://github.com/storybooks/storybook/tree/release/3.4/addons/jest
+storiesOf('Jest', module)
+  .addDecorator(withTests({ results })('sum'))
+  .add('withTests', () => <div>Jest results fri sum function in storybook</div>);
