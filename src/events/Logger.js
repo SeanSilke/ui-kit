debb9260def59c493e8c5df56f988ec8a970a39c
Sergey Orlov <OrlovMail@gmail.com>
2018-08-31 12:17:05 +0300

    EN-334 add addong events to storybook

---
+++ src/events/Logger.js
@@ -0,0 +1,62 @@
+/* eslint-disable react/no-deprecated*/
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import EventEmiter from 'eventemitter3';
+import json from 'format-json';
+
+import * as EVENTS from './events';
+
+const styles = {
+  wrapper: {
+    padding: 20,
+    fontFamily: 'Areal, sans-serif',
+    color: 'rgb(51, 51, 51)',
+  },
+  item: {
+    listStyle: 'none',
+    marginBottom: 10,
+  },
+};
+
+export class Logger extends Component {
+
+  state = {
+    events: [],
+  };
+
+  componentWillMount() {
+    const { emiter } = this.props;
+
+    emiter.on(EVENTS.TEST_EVENT_1, this.onEventHandler(EVENTS.TEST_EVENT_1));
+    emiter.on(EVENTS.TEST_EVENT_2, this.onEventHandler(EVENTS.TEST_EVENT_2));
+    emiter.on(EVENTS.TEST_EVENT_3, this.onEventHandler(EVENTS.TEST_EVENT_3));
+    emiter.on(EVENTS.TEST_EVENT_4, this.onEventHandler(EVENTS.TEST_EVENT_4));
+  }
+
+  onEventHandler = name =>
+    (payload) => {
+      this.setState(({ events }) => ({
+        events: [...events, { name, payload }],
+      }));
+    };
+
+  render() {
+    return (
+      <div style={styles.wrapper}>
+        <h1>Logger</h1>
+        <dl>
+          {this.state.events.map((event, i) => (
+            <div style={styles.item} key={i}>
+              <dt><b>Event name:</b> {event.name}</dt>
+              <dd><b>Event payload:</b> {json.plain(event.payload)}</dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    );
+  }
+}
+
+Logger.propTypes = {
+  emiter: PropTypes.instanceOf(EventEmiter).isRequired,
+};
---
+++ src/events/index.stories.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import EventEmiter from 'eventemitter3';
+import WithEvents from '@storybook/addon-events';
+
+import {Logger} from './Logger';
+import * as EVENTS from './events';
+
+const emiter = new EventEmiter();
+const emit = emiter.emit.bind(emiter);
+
+storiesOf('__example of using WithEvents', module)
+  .addDecorator(getStory => (
+    <WithEvents
+      emit={emit}
+      events={[
+        {
+          name: EVENTS.TEST_EVENT_1,
+          title: 'Test event 1',
+          payload: 0,
+        },
+        {
+          name: EVENTS.TEST_EVENT_2,
+          title: 'Test event 2',
+          payload: 'asdasdad asdasdasd',
+        },
+        {
+          name: EVENTS.TEST_EVENT_3,
+          title: 'Test event 3',
+          payload: {
+            string: 'value',
+            number: 123,
+            array: [1, 2, 3],
+            object: {
+              string: 'value',
+              number: 123,
+              array: [1, 2, 3],
+            },
+          },
+        },
+        {
+          name: EVENTS.TEST_EVENT_4,
+          title: 'Test event 4',
+          payload: [
+            {
+              string: 'value',
+              number: 123,
+              array: [1, 2, 3],
+            },
+            {
+              string: 'value',
+              number: 123,
+              array: [1, 2, 3],
+            },
+            {
+              string: 'value',
+              number: 123,
+              array: [1, 2, 3],
+            },
+          ],
+        },
+      ]}
+    >
+      {getStory()}
+    </WithEvents>
+  ))
+  .add('Logger', () => <Logger emiter={emiter} />);
