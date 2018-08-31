5f198de0adcb89779d7b6b5cbf551c7b671ed768
Sergey Orlov <OrlovMail@gmail.com>
2018-08-31 15:53:49 +0300

    EN-334 Added ddon-viewport

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,3 +3,4 @@ import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-events/register';
 import '@storybook/addon-jest/register';
+import '@storybook/addon-viewport/register';
